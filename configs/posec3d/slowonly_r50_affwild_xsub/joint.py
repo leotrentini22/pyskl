23b7f63193df89c14bedecdaffbdb66dbbde44d3
leotrentini22 <trentini.leonardo00@gmail.com>
2023-05-23 00:58:39 +0200

    changed batch size

--- configs/posec3d/slowonly_r50_affwild_xsub/joint.py
+++ configs/posec3d/slowonly_r50_affwild_xsub/joint.py
@@ -24,7 +24,7 @@ ann_file = '/home/trentini/pyskl/tools/data/AffWild_train.pkl'
 left_kp = [] #[1, 3, 5, 7, 9, 11, 13, 15]
 right_kp = [] #[2, 4, 6, 8, 10, 12, 14, 16]
 train_pipeline = [
-    dict(type='UniformSampleFrames', clip_len=48),  #cambiato qui in clip len, numero di frame da prendere in considerazione (noi sempre 1)
+    dict(type='UniformSampleFrames', clip_len=12),  #cambiato qui in clip len, numero di frame da prendere in considerazione (noi sempre 1)
     dict(type='PoseDecode'),
     dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
     dict(type='Resize', scale=(-1, 64)),
@@ -37,7 +37,7 @@ train_pipeline = [
     dict(type='ToTensor', keys=['imgs', 'label'])
 ]
 val_pipeline = [
-    dict(type='UniformSampleFrames', clip_len=48, num_clips=1),
+    dict(type='UniformSampleFrames', clip_len=12, num_clips=1),
     dict(type='PoseDecode'),
     dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
     dict(type='Resize', scale=(64, 64), keep_ratio=False),
@@ -47,7 +47,7 @@ val_pipeline = [
     dict(type='ToTensor', keys=['imgs'])
 ]
 test_pipeline = [
-    dict(type='UniformSampleFrames', clip_len=48, num_clips=1),
+    dict(type='UniformSampleFrames', clip_len=12, num_clips=1),
     dict(type='PoseDecode'),
     dict(type='PoseCompact', hw_ratio=1., allow_imgpad=True),
     dict(type='Resize', scale=(64, 64), keep_ratio=False),
