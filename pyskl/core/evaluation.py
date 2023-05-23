7b82ca0495606c7b6ff6ecc1d68bbcc7ea75ae35
leotrentini22 <trentini.leonardo00@gmail.com>
2023-05-23 02:14:59 +0200

    changed data tipe in evaluation

--- pyskl/core/evaluation.py
+++ pyskl/core/evaluation.py
@@ -123,8 +123,10 @@ def mean_class_accuracy(scores, labels):
 
 
 def statistics(pred, y, thresh):
-    batch_size = len(pred) #pred.size(0)
-    class_nb = len(pred[0]) #pred.size(1)
+    pred=torch.tensor(pred)
+    y=torch.tensor(y)
+    batch_size = pred.size(0)
+    class_nb = pred.size(1)
     pred = pred >= thresh
     pred = pred.long()
     statistics_list = []
