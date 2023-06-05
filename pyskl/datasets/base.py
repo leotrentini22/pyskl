94a3156e228535dfc8e757e02cb8bc9d0761708c
leotrentini22 <trentini.leonardo00@gmail.com>
2023-06-05 14:22:35 +0200

    typos

--- pyskl/datasets/base.py
+++ pyskl/datasets/base.py
@@ -240,7 +240,7 @@ class BaseDataset(Dataset, metaclass=ABCMeta):
                 gt_labels_arrays = gt_labels_arrays >= 0.5
                 gt_labels_arrays = gt_labels_arrays.astype(int)
                 results = np.concatenate(results)
-                f1 = f1_score(results, gt_labels_arrays, average='macro')
+                f1 = f1_score(results, gt_labels_arrays) #, average='macro')
                 eval_results['f1_score'] = f1*100.
                 log_msg = f'\nf1_score\t{f1:.4f}'
                 print_log(log_msg, logger=logger)
